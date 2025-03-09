07e2b41de5b720d3b2d50e638537c025a73093ea
G Sh <conan@yandex.ru>
2025-03-09 22:28:47 +0300

    Добавил фикстуру во все тесты

--- tests.py
+++ tests.py
@@ -8,16 +8,12 @@ class TestBooksCollector:
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 
     def test_add_new_book_add_one_book_book_added(self, collector):
-        # создаем экземпляр (объект) класса BooksCollector
-       # collector = BooksCollector()
         #добавляем книгу
         collector.add_new_book('The Shawshank Redemption')
         # проверяем, что добавилось именно одна книга
         assert len(collector.books_genre) == 1
 
-    def test_set_book_genre_book_genre_genre_added(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    def test_set_book_genre_book_genre_genre_added(self, collector):
         #добавляем книгу
         collector.add_new_book('The Shawshank Redemption')
         #устанавливаем жанр
@@ -25,9 +21,7 @@ class TestBooksCollector:
         #проверяем, что добавился жанр
         assert collector.books_genre['The Shawshank Redemption'] == 'Фантастика'
 
-    def test_set_book_genre_book_genre_genre_added(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    def test_set_book_genre_book_genre_genre_added(self, collector):
         #добавляем книгу
         collector.add_new_book('The Shawshank Redemption')
         #устанавливаем жанр
@@ -35,9 +29,7 @@ class TestBooksCollector:
         #проверяем, что добавился жанр
         assert collector.books_genre.get('The Shawshank Redemption') == 'Фантастика'
 
-    def test_get_books_with_specific_genre_book_genre_specific_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    def test_get_books_with_specific_genre_book_genre_specific_books(self, collector):
         #добавляем книгу
         collector.add_new_book('The Shawshank Redemption')
         #устанавливаем жанр
