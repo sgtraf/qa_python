a55642a81f1c5a854dee9ecd4469f821b8e1f204
G Sh <conan@yandex.ru>
2025-03-09 22:07:11 +0300

    Добавил тест test_set_book_genre_book_genre_genre_added проверка получения жанра книги по ее названию.

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 # Начинаю работать над проектом
 1. Добавил тест test_add_new_book_add_one_book_book_added
 проверка добавления новой книги.
-2. 
+2. Добавил тест test_set_book_genre_book_genre_genre_added проверки добавления жанра.
+3. Добавил тест test_set_book_genre_book_genre_genre_added проверка получения жанра книги по ее названию.
--- tests.py
+++ tests.py
@@ -15,11 +15,22 @@ class TestBooksCollector:
         # проверяем, что добавилось именно одна книга
         assert len(collector.books_genre) == 1
 
-    def test_set_book_genre_set_genre_genre_added(self):
+    def test_set_book_genre_book_genre_genre_added(self):
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
         #добавляем книгу
         collector.add_new_book('The Shawshank Redemption')
+        #устанавливаем жанр
         collector.set_book_genre('The Shawshank Redemption', 'Фантастика')
-        # проверяем, что добавился жанр
+        #проверяем, что добавился жанр
         assert collector.books_genre['The Shawshank Redemption'] == 'Фантастика'
+
+    def test_set_book_genre_book_genre_genre_added(self):
+        # создаем экземпляр (объект) класса BooksCollector
+        collector = BooksCollector()
+        #добавляем книгу
+        collector.add_new_book('The Shawshank Redemption')
+        #устанавливаем жанр
+        collector.set_book_genre('The Shawshank Redemption', 'Фантастика')
+        #проверяем, что добавился жанр
+        assert collector.books_genre.get('The Shawshank Redemption') == 'Фантастика'
