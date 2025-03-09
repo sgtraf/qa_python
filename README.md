45f233f05e9f9530c9222966a9d667193c96e421
G Sh <conan@yandex.ru>
2025-03-09 21:36:29 +0300

    Добавил тест test_add_new_book_add_one_book_book_added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # qa_python
 # Начинаю работать над проектом
+1. Добавил тест test_add_new_book_add_one_book_book_added
--- tests.py
+++ tests.py
@@ -22,3 +22,19 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    def test_add_new_book_add_one_book_book_added(self):
+        # создаем экземпляр (объект) класса BooksCollector
+        collector = BooksCollector()
+        #добавляем книгу
+        collector.add_new_book('The Shawshank Redemption')
+        # проверяем, что добавилось именно одна книга
+        assert len(collector.books_genre) == 1
+
+    def test_add_new_book_add_one_book_book_added(self):
+        # создаем экземпляр (объект) класса BooksCollector
+        collector = BooksCollector()
+        #добавляем книгу
+        collector.add_new_book('The Shawshank Redemption')
+        # проверяем, что добавилось именно одна книга
+        assert len(collector.books_genre) == 1
