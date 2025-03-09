7b866da257bbaf3e84e73f43dd82f3f51f8d8201
G Sh <conan@yandex.ru>
2025-03-09 21:48:02 +0300

    Обновил описание Обновление README.md

--- tests.py
+++ tests.py
@@ -4,22 +4,6 @@ from main import BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
-
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 
@@ -31,10 +15,3 @@ class TestBooksCollector:
         # проверяем, что добавилось именно одна книга
         assert len(collector.books_genre) == 1
 
-    def test_add_new_book_add_one_book_book_added(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-        #добавляем книгу
-        collector.add_new_book('The Shawshank Redemption')
-        # проверяем, что добавилось именно одна книга
-        assert len(collector.books_genre) == 1
