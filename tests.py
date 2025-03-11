77031f1c481e1ddb99382c6c11743f8064722d72
G Sh <conan@yandex.ru>
2025-03-11 09:01:00 +0300

    Добавлен filter  в тесте test_get_books_for_children_collecor_books_for_children

--- tests.py
+++ tests.py
@@ -33,7 +33,7 @@ class TestBooksCollector:
         #заполнем коллекцию данными
         books_fantasy = collector_full.get_books_with_specific_genre('Фантастика')
         #проверяем, что выводятся книги нужного жанра
-        assert list(filter(lambda x: collector_full.books_genre[x] != 'Фантастика', books_fantasy)) == []
+        assert list(filter(lambda x: collector_full.books_genre[x] == 'Фантастика', books_fantasy)) != []
 
     def test_get_books_genre_collecor_books_genre(self, collector_full):
         #заполнем коллекцию данными
@@ -45,11 +45,7 @@ class TestBooksCollector:
         #заполнем коллекцию данными
         books_for_children = collector_full.get_books_for_children()
         #проверяем, что выводятся книги нужного жанра
-        is_true = True
-        for item in books_for_children:
-            if collector_full.books_genre.get(item) == 'Ужасы' or collector_full.books_genre[item] == 'Детективы':
-                is_true = False
-        assert is_true
+        assert list(filter(lambda x: collector_full.books_genre[x] == 'Ужасы' or collector_full.books_genre[x] == 'Детективы', books_for_children)) == []
 
     def test_add_book_in_favorites_books_book_in_favorites(self, collector):
         #добавляем книгу
